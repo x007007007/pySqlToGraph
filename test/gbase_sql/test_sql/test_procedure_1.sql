73db94f5ae55fa1832a42d19054d55d4dc03f9d5
xxc <x007007007@hotmail.com>
2021-03-17 17:29:27 +0800

    add test sql

---
+++ test/gbase_sql/test_sql/test_procedure_1.sql
@@ -0,0 +1,9 @@
+CREATE DEFINER="a2"@"%" PROCEDURE "a1"( out  bbb int,  -- aaa
+                                        IN   aaa VARCHAR, -- ssss
+                                        IN   ccc VARCHAR,
+                                        IN   ddd  VARCHAR
+												       )
+begin
+
+
+end
