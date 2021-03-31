d789f39b6798ca07f92145247d3de26f2df2e094
xxc <x007007007@hotmail.com>
2021-03-31 14:03:49 +0800

    update GBase template form mysql

--- test/gbase_sql/base_sql/test_update.py
+++ test/gbase_sql/base_sql/test_update.py
@@ -4,12 +4,10 @@ from gbase_parser_simple.test_help import read_sql
 
 
 @pytest.mark.parametrize("sql,entry_name", itertools.product([
-    "update `table` set a = 1"
-    # "select `f`.`b` c, d e from a f, e as g;",
-    # 'select "f"."b" c, d e from a `f`, e g;',
+    "update `table` set a = 1",
 ], [
     'updateStatement',
-    # 'simpleStatement'
+    'simpleStatement'
 ]))
 def test_base_update_1(sql, entry_name):
     parser = read_sql(sql)
