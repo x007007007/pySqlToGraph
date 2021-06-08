195f07dedc9ab6217061d40871e4ee4f6e531954
xxc <x007007007@hotmail.com>
2021-06-08 22:15:13 +0800

    bug fix

--- src/gbase_parser_simple/engine/deduce.py
+++ src/gbase_parser_simple/engine/deduce.py
@@ -1,3 +1,4 @@
+import warnings
 from functools import wraps
 import logging
 import pprint
@@ -36,6 +37,26 @@ class Field:
     def order(self):
         return self._order
 
+    @property
+    def alias(self): return self._alias
+
+    @property
+    def ref_name(self):
+        if self._alias:
+            return self._alias
+        elif self._source:
+            assert len(self._source) == 1, 'unbelievable! without alias meanwhile is expr?'
+            if len(self._source) > 0:
+                return self._source[0]['name']
+        else:
+            warnings.warn("wonderful!? surrender!")
+
+    def get_source(self, table):
+        res = []
+        for s in self._source:
+            res.extend(table.find_source_of_name(ns=s['namespace'], name=s['name']))
+        return res
+
 
 class Table:
     def __init__(self, fields, dbname=None, name=None, alias=None, parent=None):
@@ -53,6 +74,13 @@ class Table:
         else:
             return f"[T{self.dbname}.{self.name}]"
 
+    @property
+    def ref_name(self):
+        if self.alias:
+            return self.alias.lower()
+        elif self.name:
+            return self.name.lower()
+
     def __repr__(self):
         cols, lines = get_terminal_size()
         filed_str = ' | '.join((repr(f) for f in self.fields))
@@ -69,6 +97,58 @@ class Table:
         head = "".join(head)
         return f"{head}\n   {filed_str}  \n{tail}"
 
+    def find_source_of_name(self, ns, name=None, o=None):
+        """
+        return source of name
+        :param ns:
+        :param name:
+        :return:
+        """
+        assert name is not None or o is not None, 'at least have name or order'
+        if self.parent:
+            return self._query_find_source_of_name(ns, name, o)
+        else:
+            return self._table_find_source_of_name(ns, name, o)
+
+    def _table_find_source_of_name(self, ns, name=None, o=None):
+        print(ns, name, o)
+        if ns == self.ref_name:
+            return [dict(db=self.dbname, table=self.name, col=name)]
+        else:
+            raise KeyError(f"{ns}, {name}, {o}")
+
+    def _query_find_source_of_name(self, ns, name=None, o=None) -> list:
+
+        if ns is None:
+            ns = self.dbname
+        if ns != self.ref_name:
+            warnings.warn(f"should't recurse into deep subquery!!!, {self.ref_name}")
+            for sub_table in self.parent:  # type: Table
+                if res := sub_table.find_source_of_name(ns, name):
+                    return res
+        field = None
+        if name is None:
+            field = self.fields[o]
+        else:
+            for field in self.fields:  # type:Field
+                if field.ref_name == name.lower():
+                    break
+        assert isinstance(field, Field)
+        sub_table_set = TableSet(self.parent)
+        res = field.get_source(sub_table_set)
+        return res
+
+
+class TableSet:
+    def __init__(self, tables):
+        self.tables = tables
+
+    def find_source_of_name(self, ns, name=None, o=None):
+        for table in self.tables:
+            if ns == table.ref_name:
+                if res := table.find_source_of_name(ns, name, o):
+                    return res
+        return None
 
 class Deduce(object):
 
@@ -77,7 +157,7 @@ class Deduce(object):
         self.session = self.graphdb.driver.session()
 
     def run(self, query):
-        print(query)
+        # print(query)
         return self.session.run(query)
 
     def root(self):
@@ -93,6 +173,7 @@ class Deduce(object):
                 getattr(self, f"action_{type_name}", None)
             )(record, record_id, record_data)
             LOGGER.info(context)
+            pprint.pprint(context)
 
     def action_create_procedure(self, record, record_id, record_data):
         # argument
@@ -127,7 +208,7 @@ class Deduce(object):
     def action_select(self, record, record_id, record_data, context):
         LOGGER.info("action_select")
 
-    def _action_update_tables(self, record_id):
+    def _action_update_tables(self, record_id) -> TableSet:
         tables_info_res = self.run(f"""
             MATCH (update:ACTION)
                 -[:Children]->(:TABLES)
@@ -140,7 +221,7 @@ class Deduce(object):
             table_data = table.data()
             table = self._table_reduce(table, table_data['id'], table_data, None)  # type:Table
             tables_list.append(table)
-        return tables_list
+        return TableSet(tables_list)
 
     def _table_reduce(self, record, record_id, record_data, context) -> Table:
         if record_data['table'].get('type') == 'subquery':
@@ -169,17 +250,18 @@ class Deduce(object):
             raise TypeError(record_data['table']['type'])
 
     def _deal_select(self, record, record_id, record_data, context) -> Table:
+        print('_deal_select')
         form_tables_node = self.run(f"""
             MATCH (select:ACTION)-[:from]->(tables:TABLES)-[:Children]->(table:TABLE)
             where id(select) = {record_id}
             return table, id(table) as id
         """)
-        table_tree = []
+        table_list = []
         for table_node in form_tables_node:
             table_data = table_node.data()
             table_id = table_data['id']
             res = self._table_reduce(table_node, table_id, table_data, None)
-            table_tree.append(res)
+            table_list.append(res)
         # pprint.pprint(table_tree)
 
         out_cols_node = self.run(f"""
@@ -199,7 +281,8 @@ class Deduce(object):
             field.out_id = out_id
             cols.append(field)
         cols.sort()
-        table = Table(fields=cols, parent=table_tree)
+        print(table_list)
+        table = Table(fields=cols, parent=table_list)
         table.graph_id = record_id
         # print(table)
         return table
@@ -214,21 +297,42 @@ class Deduce(object):
         for e_node in effect_nodes:
             e_data = e_node.data()
             e_id = e_data['id']
-            res.append(e_data['node'])
+            if node_res := e_data['node']:
+                res.append(node_res)
+
         # print(res)
         return res
 
     def action_update(self, record, record_id, record_data, context):
-        tables_list = self._action_update_tables(record_id)
-        LOGGER.info(f"action_update: {context} ------- {tables_list}")
+        table_set = self._action_update_tables(record_id)
+        LOGGER.info(f"action_update: {context} ------- {table_set}")
 
-        update_fields_result = self.run(f"""
+        write_fields = self.run(f"""
             MATCH (update:ACTION)
-                -[:Children]->(:TABLES)
-                -[:Children]->(table:TABLE)
+                -[:Children]->(:WRITE)
+                -[:Children]->(field:FIELD)
+            MATCH p=(field)<-[:effect]-(upfield:FIELD)
             where id(update) = {record_id}
-            return table, id(table) as id
+            return p, id(field) as fid, id(upfield) as ufid
         """)
+        for write_rule in write_fields:
+            write_data = write_rule.data()
+            modify_field_data, _, source_field_data = write_data['p']
+
+            modify_field = table_set.find_source_of_name(modify_field_data['ref_namespace'], modify_field_data['ref_name'])
+            source_field = table_set.find_source_of_name(source_field_data['ref_namespace'], source_field_data['ref_name'])
+
+            if modify_field and source_field:
+                source_field = [(k['db'], k['table'], k['col']) for k in source_field]
+                ref_key = "{db}.{table}.{col}".format(**(modify_field[0]))
+                if ref_key not in context['update']:
+                    context['update'][ref_key] = set(source_field)
+                else:
+                    context['update'][ref_key].update(source_field)
+
+            else:
+                warnings.warn(f"search failed {str(write_data['p'])}", UserWarning)
+
 
         # self.run(f"""
         #     MATCH (update:ACTION)
